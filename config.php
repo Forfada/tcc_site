a5ff99af1bb240b91e72bd1e283804eea8bdc36f
rafa <rafaelaraul20@gmail.com>
2025-10-24 14:32:28 -0300

    mudanças

--- config.php
+++ config.php
@@ -29,14 +29,4 @@
 	define("CSS", ABSPATH . "inc/style.css");
 	define("INIT", ABSPATH . "inc/init.php");
 
-	// SMTP / Mail settings (configure antes de usar em produção)
-	if (!defined('SMTP_HOST')) define('SMTP_HOST', 'smtp.mailgun.org');
-	if (!defined('SMTP_USER')) define('SMTP_USER', 'lunaristest@sandbox09789730a53747a79f95f02c08f33a40.mailgun.org');
-	if (!defined('SMTP_PASS')) define('SMTP_PASS', 'a6cf56e2a6e8d4e52974380c5477432e-ba8a60cd-6834d29f');
-	if (!defined('SMTP_PORT')) define('SMTP_PORT', 587);
-	if (!defined('SMTP_SECURE')) define('SMTP_SECURE', 'tls'); // 'tls', 'ssl' or ''
-	if (!defined('MAIL_FROM')) define('MAIL_FROM', 'lunaristest@sandbox09789730a53747a79f95f02c08f33a40.mailgun.org');
-	if (!defined('MAIL_FROM_NAME')) define('MAIL_FROM_NAME', 'Lunaris');
-	
-	require_once __DIR__ . '/vendor/autoload.php';
 ?>
